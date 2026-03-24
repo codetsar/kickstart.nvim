f58b48422ac97b2dfda2dee6d86ee2471ede5a11
ykotsar <ykotsar@bccipmt.com>
2026-03-24 10:39:37 +0300

    add pyright and rust-analyzer, add rounded window borders

--- init.lua
+++ init.lua
@@ -685,6 +685,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        xml = { 'xmllint' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
